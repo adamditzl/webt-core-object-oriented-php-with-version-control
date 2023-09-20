aff6e44f7e97b4582af759e884a01b2f4c8b7c65
junio <9179@htl.rennweg.at>
2023-09-20 09:03:26 +0200

    - Stand 20.09.2023 08:57: In VideoAbstract.php Constructor für den Titel und die src erstellt

--- VideoAbstract.php
+++ VideoAbstract.php
@@ -1,6 +1,9 @@
 <?php
 
-class VideoAbstract
+abstract class VideoAbstract implements Videointerface
 {
-
+    public function __construct($src, $title) {
+        this->src = $src;
+        this->src = $title;
+    }
 }
