fba05704e2c8334f90e8ca04c4bcca59ef90dd8c
Lukas Paukner <0055@htl.rennweg.at>
2023-10-15 10:44:29 +0200

    WEBT finished

--- index.php
+++ index.php
@@ -5,22 +5,59 @@ use src\Vimeo;
 
 require_once 'src/YouTube.php';
 require_once 'src/Vimeo.php';
+$videos = ''; //defining variable for later use
 
+//creating essentially a map to make the creation of the videos easier in a foreach loop
 $youtubes = array(
-            "St. Maria Goretti" => "FjuZJQdEcdg"
-        );
-$videos = '';
-foreach (array_keys($youtubes) as $array_key) {
-    $newVideo = new YouTube($array_key, $youtubes[$array_key]);
+    "St. Maria Goretti0" => "FjuZJQdEcdg",
+    "St. Maria Goretti1" => "FjuZJQdEcdg",
+    "St. Maria Goretti2" => "FjuZJQdEcdg",
+    "St. Maria Goretti3" => "FjuZJQdEcdg",
+    "St. Maria Goretti4" => "FjuZJQdEcdg",
+    "St. Maria Goretti5" => "FjuZJQdEcdg",
+    "St. Maria Goretti6" => "FjuZJQdEcdg",
+    "St. Maria Goretti7" => "FjuZJQdEcdg",
+    "St. Maria Goretti8" => "FjuZJQdEcdg"
+);
+foreach (array_keys($youtubes) as $array_key) { //foreach loop going through map with each of the keys as "$array_key"
+    $newVideo = new YouTube($array_key, $youtubes[$array_key]); //each loop creates a YouTube element with the data from the position in the map
+    //now using HEREDOC notation
     $video = <<<VIDEO
             <section>
             <p> {$newVideo->getName()}  </p>
             {$newVideo->getHTML()}  
             </section>
-VIDEO;
-    $videos .= $video;
+VIDEO; //Name of the video displayed and iframe implemented
+    $videos .= $video; //adding the part with the HEREDOC notation to previously initialized variable every time at the end of the loop
+//variables have to be in curly brackets in
+}
+
+//the same as before, just for the vimeo videos
+$vimeos = array(
+    "Paul the Glanz0" => "296219988",
+    "Paul the Glanz1" => "296219988",
+    "Paul the Glanz2" => "296219988",
+    "Paul the Glanz3" => "296219988",
+    "Paul the Glanz4" => "296219988",
+    "Paul the Glanz5" => "296219988",
+    "Paul the Glanz6" => "296219988",
+    "Paul the Glanz7" => "296219988",
+    "Paul the Glanz8" => "296219988"
+);
+
+foreach (array_keys($vimeos) as $array_key) {
+    $newVideo = new Vimeo($array_key, $vimeos[$array_key]);
+    $vimeo = <<<VIMEO
+    <section>
+        <p>{$newVideo->getName()}</p>
+        {$newVideo->getHTML()}
+    </section>
+VIMEO;
+
+    $videos .= $vimeo;
 
 }
+
 $template = <<<TEMPLATE
     <!DOCTYPE html>
     <html lang="en">
--- src/YouTube.php
+++ src/YouTube.php
@@ -9,25 +9,25 @@ class YouTube extends VideoAbstract implements Videointerface
 
     public function __construct(string $title, string $id)
     {
-        parent::__construct("YouTube", $title, $id);
+        parent::__construct("YouTube", $title, $id); //ruft Attribute vom parent ab
     }
 
-    public function getName(): string
+    public function getName(): string //gibt Namen zurück
     {
         return $this->title;
     }
 
-    public function getSource(): string
+    public function getSource(): string //src ist hier der Name der Plattform
     {
         return $this->src;
     }
 
-    public function getID(): string
+    public function getID(): string //liefert ID zurück (z.B. FjuZJQdEcdg)
     {
         return $this->id;
     }
 
-    public function getHTML(): string
+    public function getHTML(): string //macht iframe
     {
         return '<iframe src="https://www.youtube.com/embed/' . $this->getID() . '" width="360" height="203" title="' . $this->title . '" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>';
     }
